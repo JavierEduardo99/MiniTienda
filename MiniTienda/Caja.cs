f272d456d1a661e00d263431bcf30735e60c3656
Javier <javigonzalez429@gmail.com>
2024-10-11 07:34:16 -0700

    first commit

---
+++ MiniTienda/Caja.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTienda
+{
+    namespace MiniTienda
+    {
+        class Caja
+        {
+            public int Numero { get; set; }
+
+            public Caja()
+            {
+                Numero = 0;
+            }
+            public Caja(int numero)
+            {
+                Numero = numero;
+            }
+            //mostrar la lista de productos y el total del carrito
+            public void Cobrar(Carrito carrito)
+            {
+
+            }
+
+            //crear metodo pagar
+        }
+    }
+}
